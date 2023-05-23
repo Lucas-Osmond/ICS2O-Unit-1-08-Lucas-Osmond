c46d35fb7adbc9c96bc09077186850d0c533db88
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-05-23 16:48:58 +0000

    finished 6-01

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-Lucas-Osmond/sw.js", {
+    scope: "/ICS2O-Unit-1-08-Lucas-Osmond/",
+  })
+}
 
 function myButtonClicked() {
   alert("hello, world!")
